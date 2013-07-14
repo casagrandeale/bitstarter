933e73b4d6b64609932cfbd49c3ae202dc5f5985
Alejandro Casagrande <casagrandeale@gmail.com>
2013-07-14 20:14:48 -0300

    read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2 !');
+app.get('/', function(request, response) {	
+	var data = fs.readFileSync('index.html');	
+	response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
